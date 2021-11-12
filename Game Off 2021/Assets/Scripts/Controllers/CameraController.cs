c952cc5e83c8d042c0f6927671c5ce9cfb3b9190
luwai <luwaiwong@gmail.com>
2021-11-11 23:19:05 -0500

    tweaked settings

--- Game Off 2021/Assets/Scripts/Controllers/CameraController.cs
+++ Game Off 2021/Assets/Scripts/Controllers/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour
     [Header("Camera Settings")]
     [SerializeField] Transform target;
     [SerializeField] float cameraSpeed;
+    [SerializeField] float deadzone;
     [SerializeField] Vector2 offset;
     [SerializeField] float mouseFollowMagnitude;
 
@@ -18,14 +19,20 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector2 targetPosition = (Vector2)target.transform.position + offset;
 
         var mouse = Mouse.current;
         Vector2 mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(mouse.position.ReadValue());
+        if (Mathf.Abs(mousePosition.x) > deadzone || Mathf.Abs(mousePosition.y) > deadzone)
+        {
+            Vector2 mouseOffset = new Vector2(
+                Mathf.Sign(mousePosition.x) * Mathf.Sqrt(Mathf.Abs(mousePosition.x)),
+                Mathf.Sign(mousePosition.y) * Mathf.Sqrt(Mathf.Abs(mousePosition.y))) * mouseFollowMagnitude;
+            targetPosition += mouseOffset;
+        }
+        transform.position = (Vector3)Vector2.Lerp(transform.position, targetPosition, cameraSpeed) - Vector3.forward * 10;
 
-        targetPosition += new Vector2(Mathf.Sqrt(mousePosition.x), Mathf.Sqrt(mousePosition.y)) * mouseFollowMagnitude;
-        // transform.position = Vector2.Lerp(transform.position, targetPosition, cameraSpeed);
     }
 }
